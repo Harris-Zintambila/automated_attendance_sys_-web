7c38a374cfb91bb7771fa8ff1194ea6602baaaa4
Harris-Zintambila <harriszintambila9@gmail.com>
2026-04-29 03:15:46 +0200

    discard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Profile from "./pages/profile";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
@@ -16,7 +15,7 @@ const App = () => {
       <div className="flex flex-1 overflow-hidden">
         <Sidebar setCurrentPage={setCurrentPage} currentPage={currentPage} />
         {currentPage === 'dashboard' && <Dashboard />}
-        {currentPage === 'profile' && <Profile />}
+        {currentPage === 'profile' && <div>Profile Page</div>}
         {currentPage === 'assigninvigilator' && <div>Assign Invigilator Page</div>}
       </div>
     </div>
