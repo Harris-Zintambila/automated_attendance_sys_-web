2de5afbce222a3915e876cf211fff968ecf60cb4
KINGSLEY NASIMBA <bsc-com-20-22@unima.ac.mw>
2026-05-02 16:05:51 +0200

    added a dropdown on selecting role on the profile page

--- src/Profile/profile.jsx
+++ src/Profile/profile.jsx
@@ -106,7 +106,6 @@ function Profile() {
                 <option value="Mathematics">Mathematics</option>
                 <option value="Physics">Physics</option>
                 <option value="Biology">Biology</option>
-                <option value="Engineering">Engineering</option>
                 <option value="Chemistry">Chemistry</option>
               </select>
             </div>
