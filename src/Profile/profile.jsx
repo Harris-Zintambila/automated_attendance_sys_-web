fe555a5933a8191f80c91796feda10cf6a4e0254
KINGSLEY NASIMBA <bsc-com-20-22@unima.ac.mw>
2026-05-03 21:43:13 +0200

    worked on the profile table so as to test it, modified it

--- src/Profile/profile.jsx
+++ src/Profile/profile.jsx
@@ -129,7 +129,7 @@ function Profile() {
                 onChange={handleFormChange}
                 className="w-full rounded-lg border border-gray-300 px-3 py-2 text-sm"
               >
-                <option value="">Select department</option>
+                <option value="">Select Department</option>
                 <option value="Computer Science">Computer Science</option>
                 <option value="Mathematics">Mathematics</option>
                 <option value="Physics">Physics</option>
