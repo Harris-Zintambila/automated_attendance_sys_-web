b0cd19e12c29c620357f1632284744d717d72a7f
KINGSLEY NASIMBA <bsc-com-20-22@unima.ac.mw>
2026-05-05 00:46:15 +0200

    set the background colors accordingly to our system

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -200,7 +200,7 @@ function Dashboard() {
         {/* View Analytics Button */}
         <div className="mb-4">
           <Link to={getAnalyticsLink()}>
-            <button className="bg-teal-600 cursor-pointer text-white px-6 py-2 rounded-lg hover:bg-teal-700">
+            <button className="bg-teal-500 cursor-pointer text-white px-6 py-2 rounded-lg hover:bg-teal-700">
               View Analytics
             </button>
           </Link>
